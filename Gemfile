529c99224c1ae5de8135ce7fd643def922a76672
Daniel Trostli <daniel.trostli@gmail.com>
2014-01-09 16:28:48 -0200

    readded pg for heroku

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'sinatra-activerecord'
+gem 'pg'
 
 group :development, :test do
 	gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
+set :database, ENV['DATABASE_URL'] || 'postgres://localhost/App'
+
 get '/' do
 	erb :index
 end
